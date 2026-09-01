f072635fce3f1f544680b03113b4ae7b2ab0c1c0
Mabel <mabeldsouza2007@gmail.com>
2026-09-01 11:19:31 +0530

    Create page.js

---
+++ src/app/checkout/page.js
@@ -0,0 +1,9 @@
+import CartCard from '@/component/CartCard'
+
+export default function Checkout(){
+    return(
+        <div className="min-h-screen bg-gray-50 flex flex-col  items-center justify-center p-6">
+            <CartCard/>
+        </div>
+    )
+}
