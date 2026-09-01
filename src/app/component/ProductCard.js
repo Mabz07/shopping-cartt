22845c6ed31e27e6d1cde6a909dff1770e3f7d24
Mabel <mabeldsouza2007@gmail.com>
2026-09-01 11:27:27 +0530

    Refactor ProductCard component and update Home

--- src/app/component/ProductCard.js
+++ src/app/component/ProductCard.js
@@ -1,12 +1,19 @@
-import ProductCard from "@/component/ProductCard";
-import Link from 'next/link'
+"use client"
+import { products, addToCart } from '@/data/cart'
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-white text-black">
-      <h1 className = "text-3xl font-bold  text-gray-800"> PRODUCTS</h1>
-      <ProductCard/>
-      <Link href="/checkout" ><button className="mt-4 px-6 py-2 bg -black text-white rounded-lg  hover:bg-gray-800 transition-colors cursor-pointer "> Go to checkout page </button> </Link>
-    </div>
-  );
+export default function ProductCard({name, price}) {
+    return (
+        <div className="flex flex-wrap justify-center gap-6 p-4">
+            {products.map((prod) => (
+                <div key={prod.id} className='border border-gray-300 rounded-lg p-6 w-64 bg-white text-gray-800 flex flex-col justify-between'>
+                    <div>
+                    <h3 className='text-xl font-semibold mb-2'>{prod.name}</h3>
+                    <h3 className='text-lg text-gray-600 mb-4'>{prod.price}</h3>
+                    <button onClick={() => addToCart({ id: prod.id })}
+                        className = "w-full py-2 px-4 bg-blue-600 text-white font-medium rounded-md hover: bg-blue-700 transition-colors cursor-pointer"> Add to cart</button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
 }
